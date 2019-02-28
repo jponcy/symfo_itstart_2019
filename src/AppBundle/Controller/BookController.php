13969d127be75620eb4d011463d47e22513dbf09
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2019-02-28 12:09:34 +0100

    ajout creation - mauvaise facon

--- src/AppBundle/Controller/BookController.php
+++ src/AppBundle/Controller/BookController.php
@@ -6,12 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\Book;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class BookController extends Controller
 {
 
     /**
      * @Route("book")
+     * @Method("GET")
      */
     public function indexAction()
     {
@@ -36,6 +39,7 @@ class BookController extends Controller
 
     /**
      * @Route("book/{id}/show")
+     * @Method("GET")
      * Template() deprecie du a une perte de performences car il doit determiner lui meme quel est le template
      * @Template(template="book/show.html.twig") - Syntax tolere du Template
      * @param Book $book
@@ -45,5 +49,66 @@ class BookController extends Controller
         return [
             'book' => $book
         ];
+    }
+
+    /**
+     * @Route("book/create")
+     * @Method({"GET", "POST"})
+     */
+    public function createAction(Request $request)
+    {
+        $book = new Book();
+        $errors = [];
+
+        // Traitement du formulaire ; des valeurs saisies.
+        if ($request->isMethod("POST")) {
+            // Recuperation des valeurs a partir du formulaire.
+            $name = $request->request->get('name');
+            $price = $request->request->get('price');
+            $stock = $request->request->get('stock');
+            $author = $request->request->get('author');
+            $description = $request->request->get('description');
+
+            // Remplissage de l'objet.
+            $book->setName(trim($name));
+            $book->setAuthor(trim($author));
+            $book->setDescription(trim($description));
+            $book->setPrice($price ? floatval($price) : null);
+            $book->setStock($stock ? intval($stock) : null);
+
+            // Validation des saisies de l'utilisateur.
+            if ($book->getName() === '') {
+                $errors['name'] = 'Ne doit pas être vide';
+            }
+            if ($book->getDescription() === '') {
+                $errors['description'] = 'Ne doit pas être vide';
+            }
+            if ($book->getAuthor() === '') {
+                $errors['author'] = 'Ne doit pas être vide';
+            }
+            if ($book->getPrice() === null) {
+                $errors['price'] = 'Ne doit pas être vide';
+            }
+            if ($book->getStock() === null) {
+                $errors['stock'] = 'Ne doit pas être vide';
+            }
+
+            if (count($errors) === 0) { // Si pas d'erreur trouvee.
+                // Sauvegarde.
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($book);
+                $manager->flush();
+
+                // Redirection vers la liste des livres.
+                return $this->redirectToRoute('app_book_index');
+            }
+        }
+
+        // Si la methode etait GET ou des erreurs.
+        return $this->render('book/create.html.twig', [
+            'request' => $request,
+            'book' => $book,
+            'errors' => $errors
+        ]);
     }
 }
